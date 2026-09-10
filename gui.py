cc18f8318c4c8e408724d942e049c6c28959efd6
Milo <hermilocg@tec.mx>
2026-09-09 23:35:35 -0600

    sube gui de metadatos de tablas

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 from pyiceberg.catalog import load_catalog
 from nicegui import ui
+import pandas as pd
+import polars as pl
 
 ### Cargamos catálogo
 warehouse_path = "warehouse"
@@ -22,27 +24,12 @@ DATA_MAP = {
     for namespace in namespaces
 }
 
-# 2. Define the selection logic
-def handle_namespace_change(e):
-    namespace = e.value
-    # Get the sub-options corresponding to the chosen namespace
-    new_options = TABLAS.get(namespace, [])
-    
-    # Update the country dropdown's choices and clear its current value
-    tabla_select.set_options(new_options)
-    tabla_select.set_value(None)
-
-
-# 3. Define the update logic when the parent selection changes
 def handle_category_change(e):
-    # e.value contains the selected item from the parent
     selected_category = e.value
     
     if selected_category:
-        # Update the nested dropdown with corresponding subcategories
         new_options = DATA_MAP[selected_category]
         subcategory_select.set_options(new_options)
-        # Clear the old subcategory value to avoid mismatch
         subcategory_select.value = None
     else:
         subcategory_select.set_options([])
@@ -55,15 +42,38 @@ def show_selected_values():
     cat = category_select.value
     sub = subcategory_select.value
 
-    
-    ui.markdown(f'''
-    ## Example
-    This line is not .
-    This is normal text again.
 
-    {cat}
-    {sub}
-    ''').classes('w-48')
+
+    # El diálogo se crea y se abre inmediatamente al ejecutarse la función
+    with ui.dialog() as dialog, ui.card().style('width: 1000px; max-width: none; height: 800px;'):
+        table = catalog.load_table(f"{cat}.{sub}")
+        lazy_df = pl.scan_iceberg(table)
+
+        df = (
+            lazy_df
+            .head(5)
+            .collect()
+        ).to_pandas()
+
+        # Convert DataFrame to NiceGUI table format
+        columns = [{'name': col, 'label': col, 'field': col} for col in df.columns]
+        rows = df.to_dict('records')
+
+        ui.markdown(f'''
+        # **Metadatos de la Tabla**
+        - **Tabla** : `{table.properties['name']}`
+        - **Namespace** : `{table.properties['namespace']}`
+        - **Nombre** : {table.properties['longname']} 
+        - **Descripción** : {table.properties['description']}
+        - **url** : <{table.properties['url']}>
+        ''').classes('w-240')
+
+        ui.markdown("## **Data Sample**")
+        ui.table(columns=columns, rows=rows)
+        ui.button('Cerrar', on_click=dialog.close)
+    dialog.open()
+
+
         
 
 def set_allow_moving(event):
@@ -72,27 +82,25 @@ def set_allow_moving(event):
 with ui.card().classes('w-[500px] mx-auto mt-20 p-12 gap-20 rounded-2xl shadow-lg border border-gray-100 items-center'):
     ui.label('Catálogo de Datos').classes('text-h6')
     
-    # Create a horizontal row container spanning the full width
     with ui.row().classes('w-full items-start no-wrap'):
         
         with ui.column():
-            # 1. Create the parent selection
             category_select = ui.select(
                 options=list(DATA_MAP.keys()), 
                 label='Selecciona namespace',
                 on_change=handle_category_change
             ).classes('w-58')
 
-            # 2. Create the nested selection (initially empty)
             subcategory_select = ui.select(
                 options=[], 
                 label='Seleccion Tabla'
             ).classes('w-58')
 
+            with ui.dialog() as dialog, ui.card():
+                ui.label('Hello world!')
+                ui.label(category_select.value)
+                ui.button('Close', on_click=dialog.close)
+                
             ui.button('Carga Tabla', on_click=show_selected_values)
-
-          
-            
-            # Button to trigger value retrieval
 
 ui.run()
